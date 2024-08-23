bd1ef0d37f4b079649d8751a8f1adc80b6f90b2f
ArmelKamdemClemios <100877203+1armel@users.noreply.github.com>
2024-08-23 19:10:57 +0200

    [WIP]:Make the debugging wor on the STM32F49 by changing updating the launch.json file

--- Src/main.cpp
+++ Src/main.cpp
@@ -44,9 +44,7 @@ void     SystemClock_Config(void);
 
 int main(void)
 {
-  Pin pin0(0,'B') ;
-  pin0.output();
-  pin0.change();
+ 
 
 
   /* Configure the system clock to 100 MHz */
@@ -58,6 +56,9 @@ int main(void)
   /* Infinite loop */
   while (1)
   {
+    Pin pin0(12,'D') ;
+    pin0.output();
+    pin0.change();
 
   }
 }
